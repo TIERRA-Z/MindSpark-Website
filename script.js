6b456f972db7b7f98f35467dabe2c7224fec48b7
Tierra Doughty <77511842+TIERRA-Z@users.noreply.github.com>
2024-02-12 16:05:49 -0500

    Changes to homepage

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed(".typing", {
+      strings: ["Artist", "Programmer"],      
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  
+    var typed2 = new Typed(".typing-2", {
+      strings: ["Artist", "Programmer", "Creator", "Designer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
